2c17cb83f46ce226be7fa7a0bfdc10f10114cf6b
Hugo Iboa <iboazain@gmail.com>
2025-11-15 22:42:23 -0800

    Solved PrintNumbers.java using a loop to print numbers 1–10

---
+++ basics/PrintNumbers.java
@@ -0,0 +1,11 @@
+package basics;
+
+public class PrintNumbers {
+    public static void main(String[] args) {
+        int num = 0;
+        for (int i = 1; i <= 10; i++) {
+            num = i;
+            System.out.println("The number is: " + num);
+        }
+    }
+}
