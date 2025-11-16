03476b294556ad141c74989f4c593ea8610c29e1
Hugo Iboa <hiboa1@wgu.edu>
2025-11-15 21:10:59 -0800

    Add HelloPractice.java as first daily Java practice file

---
+++ basics/HelloPractice.java
@@ -0,0 +1,7 @@
+public class HelloPractice {
+    public static void main(String[] args) {
+        System.out.println("Daily Java Practice");
+        System.out.println("Coded by Hugo");
+        System.out.println("Today is: " + java.time.LocalDate.now());
+    }
+}
