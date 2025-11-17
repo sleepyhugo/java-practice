1be13b26425c48d0e92c7641ab97d0bd736f3e5f
Hugo Iboa <iboazain@gmail.com>
2025-11-17 12:06:41 -0800

    Added MultiplicationTable7.java to display the 7 times table

---
+++ basics/MultiplicationTable7.java
@@ -0,0 +1,11 @@
+package basics;
+
+public class MultiplicationTable7 {
+    public static void main(String[] args) {
+        int seven = 7;
+        for(int i = 1; i <= 10; i++){
+            int product = seven * i;
+            System.out.println(seven + " x " + i + " = " + product);
+        }
+    }
+}
