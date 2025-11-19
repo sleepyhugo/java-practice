529ce6ccce7a7ab6986b8c9c958d9545c06ae2c1
Hugo Iboa <iboazain@gmail.com>
2025-11-18 16:58:38 -0800

    Added StringBasics.java to practice common String methods

---
+++ basics/StringBasics.java
@@ -0,0 +1,11 @@
+package basics;
+
+public class StringBasics {
+    public static void main(String[] args) {
+        String sentence = "Java is fun";
+        System.out.println("Original: " + sentence);
+        System.out.println("Uppercase: " + sentence.toUpperCase());
+        System.out.println("Lowercase: " + sentence.toLowerCase());
+        System.out.println("Length: " + sentence.length());
+    }
+}
