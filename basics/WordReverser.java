9303e49447433604e3f70896d3f5ccd35dc18c4d
Hugo Iboa <iboazain@gmail.com>
2025-11-21 11:34:53 -0800

    Added mini word reversal program (loops practice).

---
+++ basics/WordReverser.java
@@ -0,0 +1,20 @@
+package basics;
+
+import java.util.Scanner;
+
+public class WordReverser {
+
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a word: ");
+        String word = scanner.next();
+
+        String reversed = "";
+
+        for (int i = word.length() - 1; i >= 0; i--){
+            reversed += word.charAt(i);
+        }
+        System.out.println("Reversed: " + reversed);
+    }
+}
