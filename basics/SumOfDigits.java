cc5f492530ce47f91dcc942f5e913b1f19d4bfcb
Hugo Iboa <iboazain@gmail.com>
2025-11-26 20:59:36 -0800

    Added SumOfDigits program to basics folder.

---
+++ basics/SumOfDigits.java
@@ -0,0 +1,23 @@
+package basics;
+
+import java.util.Scanner;
+
+public class SumOfDigits {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+
+        int sum = 0;
+
+        while (number > 0) {
+            int digit = number % 10;
+            sum += digit;
+            number = number / 10;
+        }
+
+        System.out.println("Sum of digits: " + sum);
+    }
+}
